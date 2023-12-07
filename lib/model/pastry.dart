3ae95715c6f52e93d4ce39ca90d544dc82ad34da
actiangent <hisyam.fausta@gmail.com>
2023-12-07 20:31:45 +0700

    Add model classes

---
+++ lib/model/pastry.dart
@@ -0,0 +1,18 @@
+import 'flavour.dart';
+
+class Pastry {
+  final int id;
+  final String name;
+  final double price;
+  final List<Flavour> flavours;
+
+  Pastry(this.id, this.name, this.price): flavours = List.empty();
+
+  Pastry.flavourful(this.id, this.name, this.price, this.flavours);
+
+  @override
+  int get hashCode => id;
+
+  @override
+  bool operator == (Object other) => other is Pastry && other.id == id;
+}
